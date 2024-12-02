f8b054d4b59e7e7489dc01dd38afb461c478f84b
Lukáš Majchrák <luksatv14@gmail.com>
2024-12-02 16:44:41 +0100

    ziskava startFrames, endFrames a vypocitava framy

--- PosunSubtitles/Program.cs
+++ PosunSubtitles/Program.cs
@@ -8,8 +8,13 @@ namespace PosunSUB
     {
         static void Main(string[] args)
         {
-            StreamReader reader = new StreamReader("C:\\Users\\lenovo\\Desktop\\posunSUB\\titulky.sub");
+            //PREPISTE SI TO PROSIM
+            string cesta = "C:\\Users\\lenovo\\Desktop\\posunSUB";
+
+            StreamReader reader = new StreamReader(cesta + "\\titulky.sub");
             List<string> riadky = new List<string>();
+            List<int> startFrames = new List<int>();
+            List<int> endFrames = new List<int>();
             const int fps = 25;
 
             while (true)
@@ -22,10 +27,27 @@ namespace PosunSUB
             reader.Close();
 
             Console.Write("O koľko sekúnd chcete posunúť titulky? (pri zápornych číslach sa posunú vzad): ");
-            int pocetFramov = Convert.ToInt32(Convert.ToDouble(Console.ReadLine()) * fps);
+            int pocetFramovPosunu = Convert.ToInt32(Convert.ToDouble(Console.ReadLine()) * fps);
 
-            //ziskaj startFrame, endFrame
-            
+            //ziskaj startFrames, endFrames
+            for (int i = 0; i < riadky.Count; i++)
+            {
+                startFrames.Add(Convert.ToInt32(riadky[i].Split('}')[0].Remove(0, 1)));
+                endFrames.Add(Convert.ToInt32(riadky[i].Split('}')[1].Remove(0, 1)));
+            }
+
+            //zmen framy
+            for (int i = 0; i < startFrames.Count; i++)
+            {
+                startFrames[i] += pocetFramovPosunu;
+                endFrames[i] += pocetFramovPosunu;
+            }
+
+            //vypis do konzoly zmenene riadky
+
+
+            //uloz nove data do noveho suboru
+
         }
     }
 }
